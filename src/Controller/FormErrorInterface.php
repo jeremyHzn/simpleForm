d6c4e602081219b43d118bd5817d262b8c65be59
jeremyHzn <heizmann.jeremy@gmail.com>
2023-06-07 16:32:04 +0200

    add Service

--- src/Controller/FormErrorInterface.php
+++ src/Controller/FormErrorInterface.php
@@ -2,7 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Form\FormInterface;
+
 interface FormErrorInterface
 {
+    public function saveSubmittedDataInSession(array $submittedData): void;
+
+    public function addFormErrorsInSession(FormInterface $form): void;
+
+    public function getSubmittedDataFromSession(): ?array;
+
+    public function addFormErrorsFromPreviousSubmittedDataIfExists(FormInterface $form): void;
 
 }
