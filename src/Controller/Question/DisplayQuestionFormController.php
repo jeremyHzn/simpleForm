362b92b6531ce6646294cebd1b2e9e0ba6668429
jeremyHzn <heizmann.jeremy@gmail.com>
2023-06-08 14:06:53 +0200

    add code

--- src/Controller/Question/DisplayQuestionFormController.php
+++ src/Controller/Question/DisplayQuestionFormController.php
@@ -2,7 +2,30 @@
 
 namespace App\Controller\Question;
 
+use App\Form\SatisfactionFormType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class DisplayQuestionFormController
 {
+    #[Route(path: '/', name: 'app_main', methods: ['GET'])]
+    public function getForm(): Response
+    {
 
+        $questionForm = $this->createForm(
+            type: SatisfactionFormType::class,
+            data: $this->formErrorService->getSubmittedDataFromSession(),
+            options:[
+                'action' => $this->generateUrl(route:'app_main_post'),
+                'method' => 'POST',
+            ]
+        );
+
+        // form errror
+        $questionForm->addFormErrorsFromPreviousSubmittedDataIfExists($questionForm);
+
+        return $this->render(view: 'main/index.html.twig', parameters: [
+            'questionForm' => $questionForm->createView()
+        ]);
+    }
 }
--- src/Controller/Question/ProcessQuestionFormController.php
+++ src/Controller/Question/ProcessQuestionFormController.php
@@ -2,7 +2,30 @@
 
 namespace App\Controller\Question;
 
+use App\Form\SatisfactionFormType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class ProcessQuestionFormController
 {
+    #[Route(path: '/', name: 'app_main_post', methods: ['POST'])]
+    public function postForm(Request $request): Response
+    {
+        $questionForm = $this->createForm(
+            type: SatisfactionFormType::class,
+        );
 
+        $questionForm->handleRequest($request);
+
+        // make service of form
+        if ($questionForm->isValid() === false) {
+            $formErrorService->saveSubmittedDataInSession($questionForm->getData());
+
+            $formErrorService->addFormErrorsInSession($questionForm);
+
+            return $this->redirectToRoute('app_main');
+        }
+
+    }
 }
