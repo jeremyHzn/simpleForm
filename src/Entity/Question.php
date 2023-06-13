852f76350dc5183d6a02bbefedde3c9dadee2c60
jeremyHzn <heizmann.jeremy@gmail.com>
2023-06-13 10:26:57 +0200

    baby commit

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -16,7 +16,7 @@ class Question
     private ?int $id = null;
 
     #[ORM\Column(length: 255, unique: true)]
-    private string $email;
+    private readonly string $email;
 
     #[ORM\Column(type: Types::SMALLINT, nullable: true)]
     private ?int $question1 = null;
